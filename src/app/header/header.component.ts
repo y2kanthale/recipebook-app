3f875e43c270a124e7ab1d3b53602ea95c70e985
yogesh <yogdeep.k@gmail.com>
2019-01-28 14:10:23 +0530

    lern about the header component and navigation... with EventEmitter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderAppComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderAppComponent {
+  @Output() featureSelected = new EventEmitter<string>();
+  
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+    // console.log(feature);
   }
+  constructor() { }
 
 }
